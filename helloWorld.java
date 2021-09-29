54807986a6742b2828981f204e1dc84aeb28ff9d
PRITHVI RAJ <rprithvi653@gmail.com>
2021-09-29 23:23:57 +0530

    added multiply.java

--- helloWorld.java
+++ helloWorld.java
@@ -5,5 +5,9 @@ public class helloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a = 5;
+        int b = 9;
+        int c = a+b;
+        System.out.println(c);
     }
 }
---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args)
+    {
+        int n1 = 3;
+        int n2 = 4;
+        System.out.print(n1 * n2);
+    }
+}
